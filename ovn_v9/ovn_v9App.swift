d3c1cc074e18fbfe456a306c9fb91aba187b7e25
dagangregson <dagan.gregson@gmail.com>
2021-11-02 21:28:59 +0100

    Initial Commit

---
+++ ovn_v9/ovn_v9App.swift
@@ -0,0 +1,17 @@
+//
+//  ovn_v9App.swift
+//  ovn_v9
+//
+//  Created by Dragan Larlund on 2021-11-02.
+//
+
+import SwiftUI
+
+@main
+struct ovn_v9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
